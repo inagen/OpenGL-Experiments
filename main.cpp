78471dfc9ff9f34f09c5373986f93535a11b7ef8
inagen <inagen57@gmail.com>
2018-12-08 14:13:12 +0300

    Hello, Triangle

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include "window.h"
 #include "shader.h"
@@ -7,9 +10,37 @@ using namespace MyEngine;
 int main() {
 	Window window;
 	window.create("Hello, Window!", 800, 600);
+	Shader shader("../shaders/shader.vs", "../shaders/shader.fs");
+	shader.use();
 
-	while(window.isOpen()) {
+	float vertices[] {
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO);
+
+	while(
+		window.isOpen()) {
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwPollEvents();
 		window.swapBuffers();
